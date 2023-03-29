6c4f376ae7f94238a8ee81d91ea0060da814a17a
devryyeong <thisisme20@daum.net>
2023-03-29 13:01:09 +0900

    Feat: 컬러 토큰 추가

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>가나다라마바사</p>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import GlobalStyle from "./assets/fonts";
+import GlobalFonts from "./assets/fonts";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalFonts />
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 )
