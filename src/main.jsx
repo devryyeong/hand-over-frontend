76d92acbf8063bfe60bedf39ffde3c15108509b0
devryyeong <thisisme20@daum.net>
2023-04-03 03:04:20 +0900

    Chore: 주석 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./pages/styles/GlobalStyle";
-// import GlobalFonts from "./assets/fonts/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <GlobalFonts /> */}
     <GlobalStyle />
     <App />
   </React.StrictMode>,
