b4748615b693216506106126762b5672c2025712
eunji <pih9134@naver.com>
2023-04-04 00:03:16 +0900

    Feat: Header component 개발

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import viteLogo from "./assets/logo-icon.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      <p>가나다라마바사</p>
+      <BrowserRouter>
+      <Header />
+      </BrowserRouter>
     </div>
   );
 }
