2106bc643a4c12213e977b97d03295f3f9afd3d5
eunji <pih9134@naver.com>
2023-04-25 14:52:36 +0900

    Design: svg추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Header />
-      <Category />
+        <Header />
+        <Category />
       </BrowserRouter>
     </div>
   );
