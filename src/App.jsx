2c2efa6b9501a8b39f5698214e9f520dd42e2b5f
devryyeong <thisisme20@daum.net>
2023-05-01 11:20:43 +0900

    Chore: 컨벤션 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Category from "./components/Category";
-import SP from "./components/SalesPost/SalesPost";
+import SP from "./components/SalesPost";
 
 function App() {
   return (
