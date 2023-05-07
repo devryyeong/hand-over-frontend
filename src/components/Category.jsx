7f3f0a54d40ec355171fa3213b675d4d7fccaaf1
eunji <pih9134@naver.com>
2023-05-07 22:26:21 +0900

    Feat: category button UI 변경

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,37 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-import COLORS from "../pages/styles/colors";
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import COLORS from "../pages/styles/colors";
 
 const All = styled.div`
-position: relative;
-width: 1000px;
-height: 89px;
-margin: 0px auto;
-`
-
-const Allin = styled.div`
 display: flex;
 flex-direction: row;
 align-items: flex-start;
 padding: 30px 10px 20px 15px;
 gap: 30px;
+width: 1000px;
 `
 
 const CategoryBox = styled.div.attrs({ type: "button" })`
 display: flex;
 flex-direction: row;
-align-items: center;
-padding: 10px 18px;
+align-items: flex-start;
+padding: 10px 17px 10px 20px;
 gap: 10px;
-cursor: pointer;
-background: ${(props) => props.background || `${COLORS.WHITE}`};
-border: 1px solid ${COLORS.BLUE_100};
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+background: ${COLORS.WHITE};
+border: ${(props)=>props.border || `1px solid ${COLORS.Navy_100}`};
 border-radius: 40px;
-width: 91px;
-height: 39px;
 `
 
 const BoxTxT = styled.span`
@@ -39,99 +29,88 @@ font-style: normal;
 font-weight: 700;
 font-size: 16px;
 line-height: 19px;
-display: flex;
 align-items: center;
 text-align: center;
-word-spacing:0.8em;
-color: ${(props) => props.color || `${COLORS.BLUE_100}`};
+letter-spacing: 0.2em;
+color: ${(props) => props.color || `${COLORS.GRAY}`};
 `
 
 const BtnLink = styled(Link)`
 text-decoration-line: none;
 `
 
-export const Category = () => {
-
-    const categoryTxt = [
-        {
-            id: 1,
-            txt: "전 체",
-            to: 'all'
-        },
-        {
-            id: 2,
-            txt: "숙 소",
-            to: 'hotel'
-        },
-        {
-            id: 3,
-            txt: "캠 핑",
-            to: 'camping'
-        },
-        {
-            id: 4,
-            txt: "공 연",
-            to: 'show'
-        },
-        {
-            id: 5,
-            txt: "기 타",
-            to: 'etc'
-        }
-    ]
-
+const Category = () => {
+  const categoryTxt = [
+    {
+      id: 1,
+      txt: "전 체",
+      to: ''
+    },
+    {
+      id: 2,
+      txt: "노인돌봄",
+      to: 'elderly'
+    },
+    {
+      id: 3,
+      txt: "아이돌봄",
+      to: 'kids'
+    },
+    {
+      id: 4,
+      txt: "반려동물",
+      to: 'pet'
+    },
+    {
+      id: 5,
+      txt: "기 타",
+      to: 'etc'
+    }
+  ];
+  
     //카테고리 버튼 기본값 설정
     const [selectedButton, setSelectedButton] = useState(
         parseInt(localStorage.getItem('selectedButton')) || 1
     );
 
     const handleButtonClick = (buttonId) => {
-        localStorage.setItem('selectedButton', buttonId);
         setSelectedButton(buttonId);
-    };
-
-    useEffect(() => {
-        localStorage.setItem('selectedButton', selectedButton);
-    }, [selectedButton]);
-
-    //웹사이트 나갈때
-    const handleBeforeUnload = () => {
-        localStorage.removeItem('selectedButton');
-    };
-
-    useEffect(() => {
+    
+        localStorage.setItem('selectedButton', buttonId);
+        filterResults(searchTerm);
+    
+        // URL 변경
+        const queryParam = queryString.stringify({ q: searchTerm });
+        navigate(`/?${queryParam}`, { replace: true });
+    
+      };
+      
+    
+      useEffect(() => {
         const buttonId = categoryTxt.findIndex((item) => item.to === location.pathname.substring(1));
         setSelectedButton(buttonId + 1);
-    }, [location.pathname, categoryTxt]);
-
-    useEffect(() => {
-        window.addEventListener('beforeunload', handleBeforeUnload);
-        return () => {
-            window.removeEventListener('beforeunload', handleBeforeUnload);
-        };
-    }, []);
+      }, [location.pathname, categoryTxt]);
 
     return (
         <div>
-            <All>
-                <Allin>
-                    {categoryTxt.map((item) => (
-                        <BtnLink key={item.id} to={item.to}>
-                            <CategoryBox
-                                key={item.id}
-                                onClick={() => handleButtonClick(item.id)}
-                                className={selectedButton === item.id ? 'active' : ''}
-                                background={selectedButton === item.id ? `${COLORS.BLUE_100}` : `${COLORS.WHITE}`}
-                            >
-                                <BoxTxT color={selectedButton === item.id ? `${COLORS.WHITE}` : `${COLORS.BLUE_100}`}>{item.txt}</BoxTxT>
-                            </CategoryBox>
-                        </BtnLink>
-
-                    ))}
-                </Allin>
-            </All>
+          <All>
+              {categoryTxt.map((item) => (
+                <BtnLink className="btn" key={item.id} to={item.to}>
+                  <CategoryBox
+                    key={item.id}
+                    onClick={() => handleButtonClick(item.id)}
+                    className={selectedButton === item.id ? 'active' : ''}
+                    border={selectedButton === item.id ? `1px solid ${COLORS.Navy_100}` : `1px solid ${COLORS.WHITE}`}
+                  >
+                    <BoxTxT color={selectedButton === item.id ? `${COLORS.Navy_100}` : `${COLORS.GRAY}`}>
+                      {item.txt}
+                    </BoxTxT>
+                  </CategoryBox>
+                </BtnLink>
+              ))}
+          </All>
         </div>
+      );
+    }
 
-
-    )
-}
+    export default Category;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -117,3 +117,5 @@ export const Header = ()=> {
         </div>
     )
 }
+
+export default Header;
