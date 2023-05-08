7d8aa7a0c40d688d6ed1e6b1a896437cb7d52aca
eunji <pih9134@naver.com>
2023-05-08 16:42:40 +0900

    Feat: Introduction component추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Category from "./components/Category";
 import SP from "./components/Button";
+import Introduction from "./components/Introduction";
+import AllPage from "./pages/categoryPage/AllPage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Category />
         <Routes>
-          {/* <Route path="/all" element={<AllPage />} /> */}
+          <Route element={<Layout />}>
+            <Route path="/" element={<AllPage />} />
+          </Route>
         </Routes>
         <SP />
       </BrowserRouter>
@@ -19,3 +22,15 @@ function App() {
 }
 
 export default App;
+
+const Layout = () => {
+  return (
+    <>
+      <Introduction />
+      <Category />
+      <div>
+        {<Outlet />}
+      </div>
+    </>
+  );
+}
