1b0724a5bbbb164c7274ad6974e5346e6a601183
devryyeong <thisisme20@daum.net>
2023-06-01 18:15:55 +0900

    Fix: 이메일 인증번호 확인 로직 수정

--- src/api/auth.js
+++ src/api/auth.js
@@ -14,7 +14,7 @@ export const login = async (loginInfo) => {
   return result.data.result;
 };
 
-//회원가입 API
+//회원가입 API 
 export const signUp = async (signUpInfo) => {
   return await axios.post(`${baseURL}/auth/join`, signUpInfo, {
     headers: {
@@ -50,14 +50,12 @@ export const emailConfirm = async (email) => {
 //이메일 인증번호 일치 확인 API
 export const emailCheck = async (code) => {
   const result = await axios.post(
-    `52.78.198.140/auth/join/email/check`,
+    `${baseURL}/auth/join/email/check`,
     null,
     { params: { code } },
     {
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": `*/*`,
-        "Access-Control-Allow-Credentials": "true",
       },
     },
   );
